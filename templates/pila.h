6508806606b3535f3871acd34e28ecaeb15c6944
0tomi <tomsschlotahuer@hotmail.com>
2024-08-06 00:24:35 -0300

    lopcura

---
+++ templates/pila.h
@@ -0,0 +1,27 @@
+#ifndef PILA_H
+#define PILA_H
+template <class Locura>
+class Pila{
+public:
+    Pila();
+    Pila (Locura dato);
+    void push(Locura newDato);
+    Locura pop();
+    int getSize();
+    
+private:
+    struct nodo{
+        Locura dato;
+        nodo * link;
+    };
+    nodo * frente = nullptr;
+    int size;
+};
+
+template <class Locura>
+Pila::Pila()
+{
+    size = 0;
+}
+
+#endif
