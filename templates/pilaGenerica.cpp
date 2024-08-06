7162b80d6a376f50b5dfeef9f366507e1a31a924
0tomi <tomsschlotahuer@hotmail.com>
2024-08-05 23:59:01 -0300

    Create pilaGenerica.cpp

---
+++ templates/pilaGenerica.cpp
@@ -0,0 +1,18 @@
+#ifndef "PILALOCURA"
+#define "PILALOCURA"
+template <class T>
+class PilaLocura{
+public:
+    struct nodo{
+        T dato;
+        nodo * link;
+    };
+    T pop;
+    
+
+
+private:
+
+}
+
+#endif
