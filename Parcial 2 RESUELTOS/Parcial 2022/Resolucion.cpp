e435643ad1d314d7b195974515ae1b25714ac54a
0tomi <tomsschlotahuer@hotmail.com>
2024-10-19 17:27:11 -0300

    Update Resolucion.cpp

--- Parcial 2 RESUELTOS/Parcial 2022/Resolucion.cpp
+++ Parcial 2 RESUELTOS/Parcial 2022/Resolucion.cpp
@@ -72,7 +72,7 @@ void Muebles::amortizar(){
     else this->valor *= 0.978; // Le restamos el 2.2%
 }
 
-// Gestor
+/// Gestor
 // Usaremos el gestor para leer los datos y asi inicializar las clases
 // Ademas de gestionarlas obviamente.
 class GestorBienes{
@@ -80,9 +80,9 @@ public:
     GestorBienes(const char* RutaArchivo);
     Bienes* getBien(int pos);
     void amortizarBienes();
-    // Para la consigna 2:
+    /// Para la consigna 2:
     void crearArchivo(const char* RutaArchivo);
-    // Para la consigna 3:
+    /// Para la consigna 3:
     vector<Bienes*> getBienesMayorValor();
     // Para el punto 3b vamos a usar un mapa, aunque calculo que con un struct que contenga
     // el tipo y el valor ya puede funcionar para el profe.
@@ -92,7 +92,7 @@ private:
 };
 
 /*
-    Para el tema de leer los archivos hay 2 formas.
+    Para el tema de leer los archivos hay 2 formas:
     La eficiente y la ineficiente, la diferencia 
     entre una y otra, son la cantidad de lineas que
     toman en escribirse, pero ambas son igual de faciles de 
@@ -204,10 +204,10 @@ struct Nodo{
 }
 Nodo * first;
 Nodo * last;
+T getRecursive (Nodo * nodo, int &Contador);
 
 public:
     ListaCircular(): first(nullptr), last(nullptr) {}
-    ListaCircular(T dato): first(new Nodo{first, T}), last(first) {}
     void insert(T dato);
     T get(int pos);
 };
@@ -215,7 +215,72 @@ public:
 template <class T>
 void ListaCircular<T>::insert(T dato){
     if (first == nullptr){
-        first = new Nodo{first, T};
+        first = last = new Nodo{dato, first, first};
         return;
     }
+
+    last->link = new Nodo{dato, first, last};
+    last = last->link;
+    first->back = last;
 }
+
+/*
+    La estrategia para resolver este punto que es basicamente: retornar un elemento
+    de la lista dada la posicion, y que admita posiciones negativas, 
+    va a constar en mi caso, de ir recorriendo con un contador cuantos nodos nos
+    movemos por la lista, cuando el contador llega a 0 quiere decir que encontramos
+    el nodo solicitado.
+*/
+
+template <class T>
+T ListaCircular<T>::get(int pos){
+    // Aqui podemos usar la solucion recursiva: getRecursive (first, pos);
+    // que busca el nodo de forma recursiva, pero tambien les voy a 
+    // mostrar la solucion iterativa.
+
+    int restar; bool Derecha = true; Nodo * aux = first;
+    if (pos < 0) {
+        restar = 1;
+        Derecha = false;
+    } else restar = -1;
+
+    while (pos) {
+        if (Derecha)
+            aux = aux->link;
+        else aux = aux->back;
+        pos += restar;
+    }
+    
+    // Si pos = 0, no va a entrar al while, por lo tanto devuelve el dato del primer nodo.
+    return aux->dato;
+}
+
+template <class T>
+T ListaCircular<T>::getRecursive (Nodo * nodo, int &contador){
+    if (!contador) // Es equivalente a contador == 0, porque C++ toma el 0 como false.
+        return nodo->dato;
+    
+    if (contador < 0)
+        return  getRecursive(nodo->back, ++contador);
+    else return getRecursive(nodo->link, --contador);
+}
+
+/*
+Consigna 5:
+a) ¿Qué ventajas tiene utilizar archivos binarios? ¿Qué ventajas tiene utilizar archivos
+de texto?
+    Archivos binarios: Suelen ocupar menos espacio ya que no guardan en formato caracter,
+        Se pueden leer mas rapido que los de texto ya que cuentan con un puntero.
+        Son ideales para guardar datos que no queremos que el usuario pueda ver o modificar facilmente.
+    
+    Archivos de texto: Son mas legibles, lo que los hace ideales para guardar datos que puedan ser 
+        modificados directamente desde el archivo.
+        Son mas portables, y mas simples de leer. 
+
+b) ¿Por qué se utilizan estructuras template en los lenguajes de tipado estático? ¿Cual
+es la ventaja?
+    Se usan para principalmente reutilizar codigo, y crear codigo general que no dependa
+    de las variables de tipado estatico que normalmente nos encontramos en estos lenguajes.
+    Osea que en vez de escribir una misma funcion 2 veces, la podemos escribir 1 vez 
+    pero adaptarla a el tipo de dato que queremos, usando el parametro de tipo.
+*/
